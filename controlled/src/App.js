b99c411d4053c4a44a1e95af439dbe42c93235e7
adibri <adi_nugraha.konsultan@work.bri.co.id>
2022-08-20 14:20:53 +0700

    add-more-styling

--- controlled/src/App.js
+++ controlled/src/App.js
@@ -6,30 +6,109 @@ function App() {
     loading: false,
     error: null,
   });
+  const [search, setSearch] = React.useState('');
 
   React.useEffect(() => {
     setState({ data: null, loading: true, error: null });
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    fetch('https://pokeapi.co/api/v2/pokemon/')
       .then((res) => res.json())
       .then((data) => setState({ data, loading: false, error: null }))
       .catch((error) => setState({ data: null, loading: false, error }));
   }, []);
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setState({ data: null, loading: true, error: null });
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      .then((res) => res.json())
+      .then((data) => setState({ data, loading: false, error: null }))
+      .catch((error) => setState({ data: null, loading: false, error }));
+    if (search === '') {
+      setState({ data: null, loading: false, error: null });
+    }
+  };
+
   return (
     <div>
-      <h1>LIST - P🅞KEM🅞N</h1>
+      <h1
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        LIST - P🅞KEM🅞N
+      </h1>
       <p>{error ? error.message : null}</p>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={handleSearch}
+          style={{
+            width: '70%',
+            marginBottom: '10px',
+            backgroundColor: '#282c34',
+            borderRadius: '5px',
+            color: 'white',
+            borderColor: '#ffd700',
+          }}
+        />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          style={{
+            width: '30%',
+            marginBottom: '10px',
+            backgroundColor: '#ffd700',
+          }}
+        >
+          Search
+        </button>
+      </form>
+
       {loading ? null : (
         <div>
-          {data.results?.map((item) => (
+          {data?.results?.map((item) => (
             <ul key={item.url}>
               <li>
-                <a href={item.url}>{item.name} </a>
+                {/* <a href={item.url}> */}
+                <div
+                  className="poke-box"
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    textAlign: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    height: '100px',
+                    backgroundColor: '#f5f5f5',
+                    borderRadius: '5px',
+                  }}
+                >
+                  <h2>{item.name}</h2>
+                </div>
+                {/* </a> */}
               </li>
             </ul>
           ))}
         </div>
       )}
+      {search ? (
+        <div>
+          {loading ? null : (
+            <div className="poke-rs">
+              <h3>{data?.name}</h3>
+              <img src={data?.sprites?.front_default} alt={data?.name} />
+              <p>{data?.weight}</p>
+              <p>{data?.height}</p>
+            </div>
+          )}
+        </div>
+      ) : null}
     </div>
   );
 }
