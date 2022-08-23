92f7e86030a6f371920f634a74088634f2587abe
adibri <adi_nugraha.konsultan@work.bri.co.id>
2022-08-23 18:03:35 +0700

    initial-server

--- node-nested-crypted-routed/server.js
+++ node-nested-crypted-routed/server.js
@@ -1,7 +1,26 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+} else {
+  require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+}
+
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const http = require('http');
+const { json } = require('body-parser');
 const PORT = process.env.PORT || '3303';
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-http.createServer();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Wellcome To LATIHAN API - ' + json.toString());
+});
+
+http.createServer(app).listen(PORT);
