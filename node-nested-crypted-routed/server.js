9038059a561a0b3e9e19a5badc7031f7234ab683
adibri <adi_nugraha.konsultan@work.bri.co.id>
2022-08-24 08:12:27 +0700

    setup-loger-cookie parser-morgan

--- node-nested-crypted-routed/server.js
+++ node-nested-crypted-routed/server.js
@@ -9,15 +9,37 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const http = require('http');
 const { json } = require('body-parser');
 const PORT = process.env.PORT || '3303';
+const favicon = require('serve-favicon');
+const cors = require('cors');
+const logger = require('morgan');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const multer = require('multer');
+const errorHandler = require('errorhandler');
+const cookieParser = require('cookie-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:3303',
+};
 
+app.use(cors(corsOptions));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ secret: 'jhosbush', resave: true, saveUninitialized: true }));
+app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(multer({ dest: './uploads' }).any());
+
 
 app.get('/', (req, res) => {
   res.send('Wellcome To LATIHAN API - ' + json.toString());
